b916ea7ddc0d508590b60769f6a05ee0e1386b05
Bwasabe <jixg20@gmail.com>
2022-10-30 16:33:13 +0900

    강좌는 여기까지

--- SoketServer2022/src/Server.ts
+++ SoketServer2022/src/Server.ts
@@ -1,6 +1,7 @@
 import Express, { Application } from 'express';
 import {IncomingMessage} from 'http'
 import WS, { RawData } from 'ws';
+import { tankio } from './packet'; 
 
 const App: Application = Express();
 
@@ -20,28 +21,37 @@ soketServer.on("connection", (soc: WS, req: IncomingMessage) => {
         console.log(isBinary);
         console.log(data);
         
-        let lenBuffer: Buffer = data.slice(0, 2) as Buffer;
-        let length = lenBuffer.readInt16LE();   // 16비트 정수를 읽어(ushort) - Little Endian
-        let typeBuffer: Buffer = data.slice(2, 4) as Buffer;
-        let type = typeBuffer.readInt16LE();
-        let aBuf: Buffer = data.slice(4, 8) as Buffer;
-        let bBuf: Buffer = data.slice(8, 12) as Buffer;
-        let a;
-        let b;
-        if (type == 1) // SendData
-        {
-            a = aBuf.readInt32LE();
-            b = bBuf.readInt32LE();
-        }
-        else if (type == 2) {
-            a = aBuf.readFloatLE();
-            b = bBuf.readFloatLE();
+        let length: number = (data.slice(0, 2) as Buffer).readInt16LE();
+        let code: number = (data.slice(2, 4) as Buffer).readInt16LE();   // 16비트 정수를 읽어(ushort) - Little Endian
+        let payload: Buffer = data.slice(4) as Buffer; // 4부터 끝까지 잘라내기
+        
+        if (code == 0) {
+            console.log("CMove message 도착");
+            let cMove: tankio.CMove = tankio.CMove.deserialize(payload);
+            console.log(cMove.playerId, cMove.x, cMove.y);
         }
 
-        console.log(length);
-        console.log(type);
-        console.log(a);
-        console.log(b);
+        // let lenBuffer: Buffer = data.slice(0, 2) as Buffer;
+        // let length = lenBuffer.readInt16LE();   // 16비트 정수를 읽어(ushort) - Little Endian
+        // let typeBuffer: Buffer = data.slice(2, 4) as Buffer;
+        // let type = typeBuffer.readInt16LE();
+        // let aBuf: Buffer = data.slice(4, 8) as Buffer;
+        // let bBuf: Buffer = data.slice(8, 12) as Buffer;
+        // let a;
+        // let b;
+        // if (type == 1) // SendData
+        // {
+        //     a = aBuf.readInt32LE();
+        //     b = bBuf.readInt32LE();
+        // }
+        // else if (type == 2) {
+        //     a = aBuf.readFloatLE();
+        //     b = bBuf.readFloatLE();
+        // }
+        // console.log(length);
+        // console.log(type);
+        // console.log(a);
+        // console.log(b);
     });
     // soc.send("Welcome to My Server!");
     // soc.send("Welcome to My Server222!");
